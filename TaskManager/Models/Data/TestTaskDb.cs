009ae8abb2b5a93e1afb87b336b201d67fba0d96
Илья <adult.rele@gmail.com>
2019-11-17 16:27:31 +0200

    Add TestTaskDb model

---
+++ TaskManager/Models/Data/TestTaskDb.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskManager.Models.Data
+{
+	public class TestTaskDb
+	{
+		public int Id { get; set; }
+
+		public string Title { get; set; }
+
+		public string Status { get; set; }
+
+		public string Description { get; set; }
+	}
+}
