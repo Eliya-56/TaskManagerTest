e47c298ec8d9797cf011a52b3afd6820efd9ef2b
Илья <adult.rele@gmail.com>
2019-11-17 16:35:34 +0200

    Add DbContext

---
+++ TaskManager/Models/Data/TestTaskContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskManager.Models.Data
+{
+	public class TestTaskContext : DbContext
+	{
+		public DbSet<TestTaskDb> Tasks { get; set; }
+
+		public TestTaskContext(DbContextOptions<TestTaskContext> options)
+			: base(options)
+		{
+		}
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<TestTaskDb>().HasKey(task => task.Id);
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
--- TaskManager/Startup.cs
+++ TaskManager/Startup.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TaskManager.Models.Data;
 
 namespace TaskManager
 {
@@ -23,6 +21,12 @@ namespace TaskManager
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			var connection = Configuration.GetConnectionString("DefaultConnection");
+			services.AddDbContext<TestTaskContext>(options =>
+			{
+				options.UseSqlServer(connection);
+			});
+
 			services.Configure<CookiePolicyOptions>(options =>
 			{
 				// This lambda determines whether user consent for non-essential cookies is needed for a given request.
