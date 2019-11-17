fff66912d4ff6f7455d243c5509d4735cf9e41ef
Илья <adult.rele@gmail.com>
2019-11-17 16:43:08 +0200

    Add TestTaskViewModel

---
+++ TaskManager/Models/ViewModels/TestTaskViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace TaskManager.Models.ViewModels
+{
+	public class TestTaskViewModel
+	{
+		public TestTaskViewModel(
+			int id,
+			string title,
+			int status,
+			string description)
+		{
+			Id = id;
+			Title = title;
+			Status = (TaskStatus)status;
+			Description = description;
+		}
+
+		public int Id { get; }
+
+		public string Title { get; }
+
+		public TaskStatus Status { get; }
+
+		public string Description { get; }
+	}
+}
